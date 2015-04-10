3ea46457960b45e6cfba01e06e3422a45d4bad1e
SuperDicq <ricardoprudon@gmail.com>
2015-04-10 11:25:51 +0200

    Initialized timer

--- Project Wekker/Project Wekker/wekker.asm
+++ Project Wekker/Project Wekker/wekker.asm
@@ -27,7 +27,24 @@ init:
 	out SPH, R16			;
 	ldi R16, low(RAMEND)	;
 	out SPL, R16			;
-	rcall init_timer		;
+	rcall reset				;
+
+
+	; f kristal = 11059200 en 1 sec = (256/11059200) * 43200
+	; to do a 16-bit write, the high byte must be written before the low byte !
+	; for a 16-bit read, the low byte must be read before the high byte !
+	; (p 89 datasheet)
+	ldi tmp, high(43200)
+	out OCR1AH, tmp
+	ldi tmp, low(43200)
+	out OCR1AL, tmp
+ 
+	; zet prescaler op 256 & zet timer in CTC-mode
+	ldi tmp, (1 << CS12) | (1 << WGM12) 
+	out TCCR1B, tmp
+	; enable interrupt
+	ldi tmp, (1 << OCIE1A)
+	out TIMSK, tmp
 
 // Init/reset timer to 0 sec. 
 
@@ -43,5 +60,4 @@ init_timer:
 	ldi lowHr, 0x00
 
 	ret
-
-
+ 
