863dadf9a6cdfd42b817c8099ea3cc92362a6439
janos94 <osinga.jan@gmail.com>
2015-04-14 16:50:51 +0200

    Fixed 00:00:00 time reset problem

--- Project Wekker/Project Wekker/wekker.asm
+++ Project Wekker/Project Wekker/wekker.asm
@@ -6,6 +6,7 @@
  */ 
 
 .org 0x0000
+rjmp time_init
 rjmp init
 
 .org OC1Aaddr
@@ -13,7 +14,15 @@ rjmp TIMER1_COMP_ISR ; adres ISR (Timer1 Output Compare Match)
 
 .include "m32def.inc"; include m32def file 
 
-// Kleine aanpassing, kunnen beter alles in 1 variabele zetten
+time_init: 
+
+	.def seconds = r20	;
+	.def minutes = r21	;
+	.def hours = r22	;
+
+	clr seconds 
+	clr minutes
+	clr hours 
 
 init: 
 	.def tmp = r16; Define tmp on reg 16
@@ -23,22 +32,12 @@ init:
 
 	.def status = r19	;
 
-	.def seconds = r20	;
-	.def minutes = r21	;
-	.def hours = r22	;
-
 	.def alarm = r23	;
 	.def alarm_h = r24	;
 	.def alarm_m = r25	;
 
 	.def saveSR = r26	;
 	.def flags = r27	;
-
-	.def clear = r28
-	
-	/*clr seconds
-	clr minutes
-	clr hours */
 
 	// Init stackpointer program 
 	ldi		tmp,low(RAMEND)
@@ -74,43 +73,40 @@ init:
 	ldi tmp, (1 << OCIE1A)
 	out TIMSK, tmp
 
-	// init INT1
+	/* init INT1
 	LDI		tmp, (1<<ISC11)	; INT1 genereert interrupt			
 	OUT		MCUCR, tmp		
 	LDI		tmp, (1<<INT1)	; enable INT1 (GICR)
-	OUT		GICR, tmp
+	OUT		GICR, tmp*/
 
-	// init port
+	/* init port
 	SER		tmp		; tmp = $FF
 	OUT		DDRB,	tmp	; Port B is output port (via LEDs)
-	OUT		PORTB,	tmp	; LEDs uitzetten
-	SEI				; enable all interrupts
-
-	//Clear tmp
-	CLR		tmp
+	OUT		PORTB,	tmp	; LEDs uitzetten*/
+	
+	sei			; Enable interrupts
+	clr		tmp
 
 loop: 	
-	/*cpi flags, 1
-	breq time_increment
-	//rcall time_increment*/
+	cpi flags, 1
+	breq init_time
 	jmp loop
-
-TIMER1_COMP_ISR:			; ISR wordt elke seconde aangeroepen
-	IN		saveSR, SREG	; save SREG
-	//ldi		flags, 1
-	rcall time_increment
-	OUT		SREG, saveSR	; restore SREG
-	RETI				; return from interrupt
 
 // Timer routine //////////////////////
 
-time_increment: 
+init_time: 
+	//clr seconds 
+	//clr minutes
+	//clr hours
 	rcall send_time
-	//clr flags
-	inc seconds
-	cpi seconds, 60
-	breq min_increment
-	ret
+	clr flags
+	jmp sec_increment
+
+	sec_increment:
+		inc seconds
+		cpi seconds, 60
+		breq min_increment
+		ret
 	
 	min_increment:
 		clr seconds
@@ -118,7 +114,6 @@ time_increment:
 		cpi minutes, 60
 		breq hr_increment
 		ret
-		//rjmp call_interupt
 
 		hr_increment:
 		clr minutes
@@ -126,12 +121,10 @@ time_increment:
 		cpi hours, 24
 		breq newday
 		ret
-		//rjmp call_interupt
 			
 		newday: 
 			clr hours
 			ret
-			//rjmp call_interupt
 
 output:
 	//Wait for empty transmit buffer
@@ -253,76 +246,9 @@ send_time:
 	ldi tmp, 0x80
 	RCALL output
 	RET
-	
-/*
-reset:	// Reset clock to 00:00:00.
-	ldi seconds, 0x00
-	ldi minutes, 0x00
-	ldi hours, 0x00
-	ret*/
 
-/*TIMER1_ISR:				; ISR wordt elke seconde aangeroepen
-	IN saveSR, SREG		; save SREG
-	RCALL time_increment
-	OUT	SREG, saveSR	; restore SREG
-	RETI				; return from interrupt
-
-// ISR voor INT1
-handle_INT_1:
+TIMER1_COMP_ISR:			; ISR wordt elke seconde aangeroepen
 	IN		saveSR, SREG	; save SREG
-	RCALL time_increment
-	OUT		PORTB,	seconds
-	OUT		SREG,	saveSR	; restore SREG
-	RETI				; return from interrupt*/
-
-
-	
-/*// ISR voor Timer1 van 5Hz
-TIMER1_ISR:					
-	IN saveSR, SREG	; save SREG
-
-//Timer Tellers
-	CPI flags, 2
-	BREQ TIMER1_ISR_Time
-	CPI	flags, 5
-	BREQ TIMER1_ISR_10Mark
-	JMP	TIMER1_ISR_end
-//Freq is 1Hz
-TIMER1_ISR_10Mark:
-	RCALL send_time
-	CPI status, 0
-	BRNE skip_inc
-	RCALL time_increment
-skip_inc:
-	LDI flags, 0
-	JMP	TIMER1_ISR_End
-//Freq is 1Hz met  offset van 0.5sec
-TIMER1_ISR_Time:
-	rjmp send_time
-
-TIMER1_ISR_End:
-	INC	flags
-	CPI	flags, 6
-	BREQ nullify_flags
-	//OUT PORTB,	seconds
-	OUT SREG, saveSR	; restore SREG
-	RETI				; return from interrupt
-
-nullify_flags:
-LDI		flags, 0
-JMP	TIMER1_ISR_End 
-	
-	*/
-
-/*(check0: 
-		rcall input 
-		cpi tmp, 0x02
-		breq call_interupt
-		rjmp check0
-
-check1: 
-		rcall input 
-		cpi tmp, 0x03
-		breq call_interupt
-		rjmp check1
-*/
+	ldi		flags, 1
+	OUT		SREG, saveSR	; restore SREG
+	RETI					; return from interrupt
